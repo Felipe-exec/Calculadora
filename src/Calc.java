c212e83afc60ef9b6ebb2335c47c21cb666c912d
Piton-exe <mzssacramento@gmail.com>
2022-03-30 00:25:24 -0300

    Initial commit

---
+++ src/Calc.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calc 
+{
+	//atributos
+	private List<String> lastOps = new ArrayList<String>();
+	
+	//metodos
+	
+	public Number sum(Number number1, Number number2)
+	{
+		Number answer = number1.doubleValue() + number2.doubleValue();
+		
+		String formattedResult = String.format("%.0f + %.0f = %.0f", number1, number2, answer);
+		lastOps.add(formattedResult);
+		saveOp('+', number1, number2, answer);
+		
+		return answer;
+	}
+
+	public Number subtraction(Number number1, Number number2)
+	{
+		Number answer = number1.doubleValue() - number2.doubleValue();
+		
+		String formattedResult = String.format("%.0f - %.0f = %.0f", number1, number2, answer);
+		lastOps.add(formattedResult);
+		saveOp('-', number1, number2, answer);
+		
+		return answer;
+	}
+	
+	public Number multiplication(Number number1, Number number2)
+	{
+		double answer = number1.doubleValue() * number2.doubleValue();
+		
+		String formattedResult = String.format("%.0f * %.0f = %.0f", number1, number2, answer);
+		lastOps.add(formattedResult);
+		saveOp('*', number1, number2, answer);
+		
+		return answer;
+	}
+	
+	public Number division(Number number1, Number number2)
+	{
+		double answer = number1.doubleValue() / number2.doubleValue();
+		
+		String formattedResult = String.format("%.0f / %.0f = %.0f", number1, number2, answer);
+		lastOps.add(formattedResult);
+		saveOp('/', number1, number2, answer);
+		
+		return answer;
+	}
+	
+	private void saveOp(char op, Number number1, Number number2, Number answer) 
+	{
+		String formattedResult = String.format("%.0f %s %.0f = %.0f", number1.doubleValue(), op, number2.doubleValue(), answer);
+
+		lastOps.add(formattedResult);
+		
+	}
+	
+	public List<String> getLastOps()
+	{
+		List<String> copyLastOps = new ArrayList<String>();
+		
+		
+		for (String op: lastOps)
+		{
+			copyLastOps.add(op);
+		}
+		return lastOps;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,71 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		int choice = 0;
+		do
+		{
+			//recebendo variaveis e escaneando
+			double n1, n2;
+			Scanner read = new Scanner(System.in);
+			System.out.print("Digite um valor: ");
+			n1 = read.nextDouble();
+			System.out.print("Digite outro valor: ");
+			n2 = read.nextDouble();
+			
+			//menu---------------------------------------
+			System.out.println("1 - Soma");
+			System.out.println("2 - Subtração");
+			System.out.println("3 - Multiplicação");
+			System.out.println("4 - Divisão");
+			System.out.println("5 - Sair");
+			System.out.print("Sua escolha: ");
+			choice = read.nextInt();
+			//-------------------------------------------
+			
+			Calc calculator = new Calc(); //chamando a classe Calc
+			switch(choice)
+			{
+			case 1:
+				//soma-------------------------------------------------
+				System.out.print("Somatorio: " + n1 + " + " + n2 + " = ");
+				System.out.print(calculator.sum(n1, n2));
+				System.out.println();
+				break;
+			case 2:
+				//subtracao---------------------------------------------
+				System.out.print("Subtracao: " + n1 + " - " + n2 + " = " );
+				System.out.print(calculator.subtraction(n1, n2));
+				System.out.println();
+				break;
+			case 3:
+				//multiplicacao-------------------------------------------
+				System.out.print("Multiplicacao: " + n1 + " x " + n2 + " = " );
+				System.out.print(calculator.multiplication(n1, n2));
+				System.out.println();
+				break;
+			case 4:
+				//divisao------------------------------------------------
+				System.out.print("Divisao: "  + n1 + " / " + n2 + " = " );
+				System.out.print(calculator.division(n1, n2));
+				System.out.println();
+				break;
+			case 5:
+				//sair----------------------------------------------------
+				List<String> ops = calculator.getLastOps();
+				System.out.println("Operações realizadas: ");
+				for (String op : ops) 
+				{
+					System.out.println(op);
+				}
+				break;
+			}
+			System.out.println("----------------------------------------");
+			System.out.println("----------------------------------------");
+		}while(choice != 5);
+	}
+}
